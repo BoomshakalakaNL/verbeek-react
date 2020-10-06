5ed8e0fff37fa39302571d56e443502fd17c7ee3
BoomshakalakaNL <boomshakalaka.nl@gmail.com>
2020-10-07 09:54:18 +1000

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './styles/index.scss';
+import './styles/bootstrap-grid.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Home from './pages/Home';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Verbeek = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+ReactDOM.render(<Verbeek />, document.querySelector("#root"));
---
+++ src/pages/Home.js
@@ -0,0 +1,104 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import "../styles/home.scss";
+import portrait from "../assets/images/bw_portrait.jpg";
+
+const Home = () => {
+    const name = "Scott Verbeek";
+    const title = "Software Engineer";
+    const socials = [
+        {
+            name: "Email",
+            link: "mailto:srpverbeek@gmail.com",
+        },
+        {
+            name: "LinkedIn",
+            link: "https://www.linkedin.com/in/scott-verbeek-3618a2169/",
+        },
+        {
+            name: "Facebook",
+            link: "https://www.facebook.com/scott.verbeek.14/",
+        },
+    ];
+    const description =
+        "I'm a Software Engineer based in Australia, specializing in web development, currently looking for new employment. Everything on this website is built and designed by me, find our more about my [projects](/projects).";
+
+    // the following lines are for the height of the row
+    // on mobile broser.
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    window.addEventListener("resize", () => {
+        document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
+    });
+
+    return (
+        <>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-xs-12 col-lg-10 p-0 pl-4 pr-4 mt-4">
+                        <Header name={name} title={title} />
+                    </div>
+                    <div className="col-xs-12 col-lg-10 p-0 pl-4 pr-4 mt-auto mb-4">
+                        <Main description={description} />
+                        <Footer socials={socials} />
+                    </div>
+                </div>
+            </div>
+            <div className="portrait">
+                <img src={portrait} alt="" />
+            </div>
+        </>
+    );
+};
+
+const Header = ({ name, title }) => (
+    <header>
+        <h1 className="animation__slide-fade-in">
+            <span className="name">{name}</span>
+            <span className="title">{title}</span>
+        </h1>
+    </header>
+);
+
+const Main = ({ description }) => (
+    <main className="mb-4">
+        <p className="animation__slide-fade-in">
+            <ReactMarkdown source={description} />
+        </p>
+    </main>
+);
+
+const Footer = ({ socials }) => {
+    var socialLinks = " ";
+    if (socials.length) {
+        socialLinks = socials.map((social) => {
+            return <SocialLink name={social.name} link={social.link} />;
+        });
+    }
+
+    return (
+        <footer className="animation__slide-fade-in">
+            <span>
+                <b>Let's connect</b>
+            </span>
+            <span>{socialLinks}</span>
+        </footer>
+    );
+};
+
+const SocialLink = ({ name, link, newTab = true }) => {
+    if (!newTab)
+        return (
+            <a href={link} className="social">
+                {name}
+            </a>
+        );
+
+    return (
+        <a href={link} className="social" target="blank">
+            {name}
+        </a>
+    );
+};
+
+export default Home;
